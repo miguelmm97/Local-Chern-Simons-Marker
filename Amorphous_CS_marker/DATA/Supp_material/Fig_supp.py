b89732ae0c1e3398b8d910371edbcd617515b5f0
Miguel <miguelmartinezmiquel@gmail.com>
2022-10-31 16:07:43 +0100

    Final

--- Amorphous_CS_marker/DATA/Supp_material/Fig_supp.py
+++ Amorphous_CS_marker/DATA/Supp_material/Fig_supp.py
@@ -10,22 +10,22 @@ import h5py
 # Import data
 outdir="."
 for file in os.listdir(outdir):
-    if file == "Sup_material_avmarker.h5":
+    if file == "Sup_material2_avmarker.h5":
         with h5py.File(file, 'r') as f:
             datanode = f['data']
             av_marker = datanode[()]
 
-    if file == "Sup_material_marker.h5":
+    if file == "Sup_material2_marker.h5":
         with h5py.File(file, 'r') as f:
             datanode = f['data']
             marker = datanode[()]
 
-    if file == "Sup_material_xaxis.h5":
+    if file == "Sup_material2_xaxis.h5":
         with h5py.File(file, 'r') as f:
             datanode = f['data']
             x_axis = datanode[()]
 
-    if file == "Sup_material_yaxis.h5":
+    if file == "Sup_material2_yaxis.h5":
         with h5py.File(file, 'r') as f:
             datanode = f['data']
             y_axis = datanode[()]
@@ -53,6 +53,6 @@ cbar.set_label(label='$\\nu$', size=25, labelpad=-15, y=0.5)
 ax.set_xlim(-0.5, 12 - 0.5)
 ax.set_ylim(-0.5, 12 - 0.5)
 
-plt.savefig("Sup_material.pdf", bbox_inches="tight")
+plt.savefig("Sup_material2.pdf", bbox_inches="tight")
 plt.show()
 
